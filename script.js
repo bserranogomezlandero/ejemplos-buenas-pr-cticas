28707ab05849aef4fcf36fd627b66292e5517539
bserranogomezlandero <bserranogomezlandero@deloitte.es>
2021-08-30 14:56:58 +0200

    3 files modified, 1 added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello!')
+const eventPropagationContainers = document.querySelectorAll(
+  '.event-propagation-container'
+);
+
+function showText(ev) {
+  // console.log(ev.target.dataset.text);
+  console.log(ev.currentTarget.dataset.text);
+}
+
+eventPropagationContainers.forEach(container =>
+  container.addEventListener('click', showText)
+);
+
+// eventPropagationContainers[0].addEventListener('click', showText);
+// eventPropagationContainers[1].addEventListener('click', showText, true);
+// eventPropagationContainers[2].addEventListener('click', showText);
